5d60dc0baa0bda601c32495ee580daca8bbc2fbe
Aman3522-dev <Amanpal3546@gmail.com>
2025-02-27 09:45:35 +0530

    Playing with the routes

--- src/app.js
+++ src/app.js
@@ -2,23 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.get("/users", (req, res) => {
+app.get("/users/:userId/:name/:password", (req, res) => {
+  console.log(req.params);
   res.send({firstName: "aman", lastName: "Pal"})
 });
-
-app.post("/users", (req, res) => {
-  res.send("save the data successfully in db");
-});
-
-app.delete("/users", (req, res) => {
-  res.send("Deleted the data successfully");
-});
-
-// This will handles all HTTP methods API calls
-app.use("/hello", (req, res) => {
-  res.send("hello hello hello");
-});
-
 
 app.listen(7777, () => {
   console.log("server runnig on local host 7777");
