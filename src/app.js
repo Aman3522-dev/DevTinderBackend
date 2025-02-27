fa0e0c5c49470a4e36e3c2c2daba9c21b6db3784
Aman3522-dev <Amanpal3546@gmail.com>
2025-02-27 10:56:28 +0530

    Mulitple Route Handlers

--- src/app.js
+++ src/app.js
@@ -2,10 +2,16 @@ const express = require('express');
 
 const app = express();
 
-app.get("/users/:userId/:name/:password", (req, res) => {
-  console.log(req.params);
-  res.send({firstName: "aman", lastName: "Pal"})
+app.use("/users", (req, res, next) => {
+  console.log("Handling the routes");
+  next();
+  res.send("Responsible");
+}, (req, res) => {
+  console.log("Handling the routes 2");
+  res.send("2nd Responsible");
 });
+
+
 
 app.listen(7777, () => {
   console.log("server runnig on local host 7777");
