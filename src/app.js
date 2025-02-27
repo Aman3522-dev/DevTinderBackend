71cf125c42b8fc2e15a0f6ba37814c289da898cb
Aman3522-dev <Amanpal3546@gmail.com>
2025-02-27 09:02:37 +0530

    expolring GET, POST, DELETE

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const app = express();
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.get("/users", (req, res) => {
+  res.send({firstName: "aman", lastName: "Pal"})
+});
+
+app.post("/users", (req, res) => {
+  res.send("save the data successfully in db");
+});
+
+app.delete("/users", (req, res) => {
+  res.send("Deleted the data successfully");
+});
+
+// This will handles all HTTP methods API calls
+app.use("/hello", (req, res) => {
+  res.send("hello hello hello");
+});
+
+
+app.listen(7777, () => {
+  console.log("server runnig on local host 7777");
+});
