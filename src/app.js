7172376234ea0b6093ae4b2c4f52ca0e5afa3101
Aman3522-dev <Amanpal3546@gmail.com>
2025-03-02 16:57:48 +0530

    delete and Patch API

--- src/app.js
+++ src/app.js
@@ -44,8 +44,33 @@ app.get("/feed", async (req, res) => {
   catch (err) {
     res.status(400).send("Something went wrong");
   }
-
 });
+
+// Delete an user from the database
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    // const user = await User.findByIdAndDelete({_id: userId}); 
+    const user = await User.findByIdAndDelete(userId);
+    res.send("user Deleted successfully");
+  }
+  catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+// Update an user from the database
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    await User.findByIdAndUpdate({_id:userId}, data);
+    res.send("user data successfully updated");
+  }
+  catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+})
 
 connectDB()
   .then(() => {
