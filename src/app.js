53f16c1de9bd5899cfdb3fc72bfd31f815d0a4d8
Aman3522-dev <Amanpal3546@gmail.com>
2025-03-02 15:50:19 +0530

    get API

--- src/app.js
+++ src/app.js
@@ -9,12 +9,42 @@ app.post("/signup", async (req, res) => {
   //creating a new instance of User model
   const user = new User(req.body);
 
+
+
   try {
   await user.save();
   res.send("user added successfully");
   } catch (err) {
     res.status(400).send("Error saving the error: " +  err.message);
   }
+});
+
+// GET user by email
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+
+  try {
+    const users = await User.find({emailId : userEmail});
+    if(users.length === 0) {
+      res.status(404).send("user not found");
+    }
+    res.send(users);
+  }
+  catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+})
+// Feed API GET /Feed - Get all the Users from the database
+app.get("/feed", async (req, res) => {
+  
+  try {
+    const users = await User.find({});
+    res.send(users);
+  }
+  catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+
 });
 
 connectDB()
