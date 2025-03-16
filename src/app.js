888c94ddbf6c0b71d65adcac59935a347f77cc10
Aman3522-dev <Amanpal3546@gmail.com>
2025-03-16 16:45:55 +0530

    schema validations

--- src/app.js
+++ src/app.js
@@ -64,11 +64,14 @@ app.patch("/user", async (req, res) => {
   const userId = req.body.userId;
   const data = req.body;
   try {
-    await User.findByIdAndUpdate({_id:userId}, data);
+    const user = await User.findByIdAndUpdate({_id:userId}, data, {
+      returnDocument: "after",
+      runValidators: true,
+    });
     res.send("user data successfully updated");
   }
   catch (err) {
-    res.status(400).send("Something went wrong");
+    res.status(400).send("Update failed" + err.message);
   }
 })
 
--- src/models/user.js
+++ src/models/user.js
@@ -2,24 +2,48 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
     firstName: {
-        type: String
+        type: String,
+        required: true,
     },
     lastName: {
-        type: String
+        type: String,
     },
-    emailId : {
-        type: String
+    emailId: {
+        type: String,
+        lowercase: true,
+        required: true,
+        unique: true,
+        trim: true,
     },
-    password : {
-        type: String
+    password: {
+        type: String,
+        required: true,
     },
     age : {
-        type: Number
+        type: Number,
+        min: 18,
     },
     gender : {
-        type: String
-    }
+        type: String,
+        validate(value){
+            if (!["male", "female", "other"].includes(value)){
+                throw new Error("Gender is not valid");
+            }
+        },
+    },
+    photoUrl: {
+        type: String,
+        default: "https://hips.hearstapps.com/hmg-prod/images/professional-bodybuilder-arnold-schwarzenegger-posing-at-news-photo-1567803748.jpg?resize=980:*",
+    },
+    about: {
+        type: String,
+        default: "This is an default use case",
+    },
+    skills: {
+        type: [String],
+    },
+}, {
+    timestamps: true,
 });
-
 
 module.exports = mongoose.model("User", userSchema);
